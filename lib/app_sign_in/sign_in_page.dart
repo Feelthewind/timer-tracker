51ca376e183c26566bad3c3086ca7c239bb7e517
임창수 <imchangsu@imchangsuui-MacBookPro.local>
2020-06-28 16:40:16 +0900

    text, textstyle and fontweight

--- lib/app_sign_in/sign_in_page.dart
+++ lib/app_sign_in/sign_in_page.dart
@@ -8,6 +8,41 @@ class SignInPage extends StatelessWidget {
         title: Text('Time Tracker'),
         elevation: 2.0,
       ),
+      body: _buildContent(),
+    );
+  }
+
+  Widget _buildContent() {
+    return Padding(
+      padding: EdgeInsets.all(16.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Text(
+            'Sign in',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 32.0,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          SizedBox(height: 8.0),
+          Container(
+            color: Colors.red,
+            child: SizedBox(
+              height: 100.0,
+            ),
+          ),
+          SizedBox(height: 8.0),
+          Container(
+            color: Colors.purple,
+            child: SizedBox(
+              height: 100.0,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
